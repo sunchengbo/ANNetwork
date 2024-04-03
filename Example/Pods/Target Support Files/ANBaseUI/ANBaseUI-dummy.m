663451318205b173ab7420046e789d5f520bfbf1
sunchengbo <594744124@qq.com>
2024-04-03 23:13:59 +0800

    ~ 0.0.2

---
+++ Example/Pods/Target Support Files/ANBaseUI/ANBaseUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ANBaseUI : NSObject
+@end
+@implementation PodsDummy_ANBaseUI
+@end
--- Example/Pods/Target Support Files/Pods-ANNetwork_Example/Pods-ANNetwork_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ANNetwork_Example/Pods-ANNetwork_Example-frameworks.sh
@@ -176,10 +176,20 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ANBaseUI/ANBaseUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ANNetwork/ANNetwork.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/HandyJSON/HandyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ANBaseUI/ANBaseUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ANNetwork/ANNetwork.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/HandyJSON/HandyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
